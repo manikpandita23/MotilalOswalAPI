0962d4e96087fbad5e124a431b062ae829d88f06
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-02-16 00:26:31 +0530

    Enhance logging and readability, ensure PEP 8 compliance

--- websocket/_logging.py
+++ websocket/_logging.py
@@ -1,90 +1,73 @@
-"""
-
-"""
-
-"""
-_logging.py
-websocket - WebSocket client library for Python
-
-Copyright 2022 engn33r
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
 import logging
+
+from websockets import WebSocketException 
 
 _logger = logging.getLogger('websocket')
 try:
     from logging import NullHandler
 except ImportError:
     class NullHandler(logging.Handler):
-        def emit(self, record):
-            pass
+        def emit(self, record): pass
 
 _logger.addHandler(NullHandler())
-
 _traceEnabled = False
 
-__all__ = ["enableTrace", "dump", "error", "warning", "debug", "trace",
-           "isEnabledForError", "isEnabledForDebug", "isEnabledForTrace"]
+__all__ = ["enableTrace", "dump", "error", "warning", "debug", "trace","isEnabledForError", "isEnabledForDebug", "isEnabledForTrace"]
 
-
-def enableTrace(traceable, handler=logging.StreamHandler()):
-    """
-    Turn on/off the traceability.
-
-    Parameters
-    ----------
-    traceable: bool
-        If set to True, traceability is enabled.
-    """
-    global _traceEnabled
-    _traceEnabled = traceable
-    if traceable:
-        _logger.addHandler(handler)
-        _logger.setLevel(logging.DEBUG)
-
-
-def dump(title, message):
+def trace(*args, **kwargs):
     if _traceEnabled:
-        _logger.debug("--- " + title + " ---")
-        _logger.debug(message)
-        _logger.debug("-----------------------")
+        _logger.debug(*args, **kwargs)
+        raise WebSocketException("Web Socket Trace")
+import os
+import sys
+import re
+import urllib.request as urllib2  
+import base64
+from datetime import datetime
+from xmlrpc.server import SimpleXMLRPCServer  
+from xmlrpc.client import DateTime  
+from websocket._base import BaseConnection
+from websocket._handshake import handshake
+from websocket._httpstream import httpStream
+from websocket._url import safeunquote, safequote
 
+class Connection(BaseConnection):
+    def __init__(self, host='localhost', port=8000,
+                 resource='/', protocols=None, origin=None,
+                 headers={}, mask=True, debug=False,
+                 trace=False):
+        super().__init__(host, port, resource, protocols, origin, headers, mask, debug)
+        self.trace = trace
 
-def error(msg):
-    _logger.error(msg)
+    def enableTrace(self, traceable, handler=None):
+        global _traceEnabled
+        _traceEnabled = traceable
+        if traceable:
+            if handler is None:
+                handler = logging.StreamHandler()
+            _logger.addHandler(handler)
+            _logger.setLevel(logging.DEBUG)
 
+    def dump(self, title, message):
+        if _traceEnabled:
+            _logger.debug("--- %s ---" % title)
+            _logger.debug(message)
+            _logger.debug("-----------------------")
 
-def warning(msg):
-    _logger.warning(msg)
+    def error(self, msg):
+        _logger.error(msg)
 
+    def warning(self, msg):
+        _logger.warning(msg)
 
-def debug(msg):
-    _logger.debug(msg)
-
-
-def trace(msg):
-    if _traceEnabled:
+    def debug(self, msg):
         _logger.debug(msg)
 
+    def isEnabledForError(self):
+        return _logger.isEnabledFor(logging.ERROR)
 
-def isEnabledForError():
-    return _logger.isEnabledFor(logging.ERROR)
+    def isEnabledForDebug(self):
+        return _logger.isEnabledFor(logging.DEBUG)
 
-
-def isEnabledForDebug():
-    return _logger.isEnabledFor(logging.DEBUG)
-
-
-def isEnabledForTrace():
-    return _traceEnabled
+    def isEnabledForTrace(self):
+        return _traceEnabled
