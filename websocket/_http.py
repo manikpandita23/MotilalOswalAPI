ed52fffb8fa04e57c2863d8daa579ffa29d2435e
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-02-20 00:20:47 +0530

    SOCKS Proxy handling

--- websocket/_http.py
+++ websocket/_http.py
@@ -1,21 +1,3 @@
-"""
-_http.py
-websocket - WebSocket client library for Python
-
-Copyright 2022 engn33r
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
 import errno
 import os
 import socket
@@ -58,7 +40,6 @@ class proxy_info:
             self.auth = options.get("http_proxy_auth", None)
             self.no_proxy = options.get("http_no_proxy", None)
             self.proxy_protocol = options.get("proxy_type", "http")
-            # Note: If timeout not specified, default python-socks timeout is 60 seconds
             self.proxy_timeout = options.get("timeout", None)
             if self.proxy_protocol not in ['http', 'socks4', 'socks4a', 'socks5', 'socks5h']:
                 raise ProxyError("Only http, socks4, socks5 proxy protocols are supported")
